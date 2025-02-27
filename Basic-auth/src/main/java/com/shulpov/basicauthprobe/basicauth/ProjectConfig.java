fbd72c99af54c1401f205bba946f2572e29bcc23
victor <vshulpov@gmail.com>
2025-02-27 22:09:50 +0700

    basic-auth: настройка аутентификации явно, пока работает так же

--- Basic-auth/src/main/java/com/shulpov/basicauthprobe/basicauth/ProjectConfig.java
+++ Basic-auth/src/main/java/com/shulpov/basicauthprobe/basicauth/ProjectConfig.java
@@ -2,14 +2,32 @@ package com.shulpov.basicauthprobe.basicauth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
+
+    // Для настройки обработки аутентификации авторизации
+    @Bean
+    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        // Customizer - контракт, который нужно реализовывать для определения настройки любого элемента Spring Security:
+        // аутентификация, авторизация, CSRF, CORS...
+        http.httpBasic(Customizer.withDefaults());// Поведение по умолчанию, даже если бы явно не написали
+        http.authorizeHttpRequests(
+                // Аутентификация для всех запросов включена
+                c -> c.anyRequest().authenticated()
+                // Аутентификация для всех запросов выключена
+//                c -> c.anyRequest().permitAll()
+        );
+        return http.build();
+    }
 
     // Если переопределяем userDetailsService, то и passwordEncoder должны тоже переопределить
     @Bean
